b067c32ecc17c29fe14a9c68633886dbc7eb3479
seob <seobew@gmail.com>
2021-07-19 15:45:17 +0900

    chore: dist

--- libs/design-system/dist/components/MenuItem.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface MenuItemProps {
-    text: string;
-}
-export declare const MenuItem: ({ text }: MenuItemProps) => JSX.Element;
-export {};
--- libs/design-system/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Header } from './Header';
-import { Thing } from './Thing';
-import { Sidebar } from './Siderbar';
-import { Card } from './Card';
-import { Layout } from './Layout';
-export { Header, Thing, Sidebar, Card, Layout };
